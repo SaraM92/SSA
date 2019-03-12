583143e71ee47f101dfccb1392c1e69a71a130ae
Sara A. Metwalli <sara@cad.ict.e.titech.ac.jp>
2019-03-12 14:00:12 +0900

    Create SSA.py

---
+++ SSA.py
@@ -0,0 +1,19 @@
+############################################################
+#This scrip is written by:
+#Sara A. Metwalli
+#For Hara Lab in Tokyo Institute of Technology
+#
+# This is to Automatically excute the scripts
+###########################################################
+
+#STEP ONE: CLEAN UP THE EXPRESSION
+
+import cleanup
+
+#STEP TWO: CONVERT THE CLEANED UP VERSION TO NORMAL MATH
+
+import converting
+
+#STEP THREE: ANALYZE THE CONVERTED RESULTS
+
+import analyze
